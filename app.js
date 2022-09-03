3104e16f06ce9ddab6efa4644586e618247308ca
Sushmita <sushmitakumari363@gmail.com>
2022-09-04 00:37:51 +0530

    feat-variables to capture input

---
+++ app.js
@@ -0,0 +1,9 @@
+var input=document.querySelectorAll('.angle-input');
+var checkBtn=document.querySelector('#check-btn');
+var outputDiv=document.querySelector('#output-div');
+
+function isTriangle(){
+
+}
+
+checkBtn.addEventListener('click',isTriangle);
