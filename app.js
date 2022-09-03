b7509757469b383a4abd2a18fe7056ed3e9637fb
Sushmita <sushmitakumari363@gmail.com>
2022-09-04 01:02:25 +0530

    triangle quiz setup

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ var input=document.querySelectorAll('.angle-input');
 var checkBtn=document.querySelector('#check-btn');
 var outputDiv=document.querySelector('#output-div');
 
-function isTriangle(){
+function computeSumOfAngles(angle1,angle2,angle3){
+    // console.log(typeof angle1);
+    var sum=angle1+angle2+angle3;
+    return sum;
+}
 
+function isTriangle(){
+  var sumofangles=computeSumOfAngles(Number(input[0].value),Number(input[1].value),Number(input[2].value));
+//   console.log(sumofangles);
+    if(sumofangles===180)
+    {
+        outputDiv.innerHTML="Yay it's a triangle";
+    }
+    else
+    {
+        outputDiv.innerHTML="It's not a triangle";
+    }
 }
 
 checkBtn.addEventListener('click',isTriangle);
