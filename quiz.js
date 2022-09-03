63101f24ca78e09c82fd3739f3f1b80004821f0d
Sushmita <sushmitakumari363@gmail.com>
2022-09-04 02:02:07 +0530

    function computescore

--- quiz.js
+++ quiz.js
@@ -2,3 +2,19 @@ var quizForm=document.querySelector('.quiz-form');
 var submitBtn=document.querySelector('#submit-btn');
 var outputDiv=document.querySelector('#output-div');
 
+const correctAnswers=["90°","right angled","one right angle","14, 15, 26","Equilateral triangle","85° ","30°","a + b + c","two ","45°"];
+
+function calculateScore(){
+    let score=0;
+    let index=0;
+  const formResults= new FormData(quizForm);
+  for(let value of formResults.values()){
+    if(value===correctAnswers[index]){
+        score=score+1;
+    }
+    index=index+1;
+  }
+  outputDiv.innerText="Your Score is " +score;
+}
+
+submitBtn.addEventListener('click',calculateScore);
