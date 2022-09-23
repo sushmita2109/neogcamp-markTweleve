ae73d7b879cdbba551354a52c642faee976cabd7
Sushmita <sushmitakumari363@gmail.com>
2022-09-24 01:55:57 +0530

    Updated score

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ var quizForm=document.querySelector('.quiz-form');
 var submitBtn=document.querySelector('#submit-btn');
 var outputDiv=document.querySelector('#output-div');
 
-const correctAnswers=["90°","right angled","one right angle","14, 15, 26","Equilateral triangle","85° ","30°","a + b + c","two ","45°"];
+const correctAnswers=["90°","right angled","one right angle","14, 15, 26","Equilateral triangle","85°","30°","a + b + c","two ","45°"];
 
 function calculateScore(){
     let score=0;
