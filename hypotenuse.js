572db45b21036ef8564785a8d59ee8bce5806ff3
Sushmita <sushmitakumari363@gmail.com>
2022-09-24 23:24:50 +0530

    updated error handling

--- hypotenuse.js
+++ hypotenuse.js
@@ -3,13 +3,27 @@ var calculateHypotenuseBtn=document.querySelector('#calculate-hypotenuse-btn');
 var outputDiv=document.querySelector('#output-div');
 
 function calculatesqrt(side1,side2){
-     let result=Math.sqrt(side1*side1+side2*side2);
-     return result;
+    if(side1<0 ||side2<0){
+        return 0;
+    }
+    else
+    {
+        let result=Math.sqrt(side1*side1+side2*side2);
+        return result;
+    }
+    
 }
 
 function calculateHypotenus(){
     let sqrtvalue=calculatesqrt(Number(input[0].value),Number(input[1].value));
-    outputDiv.innerText=Math.trunc(sqrtvalue);
+    if(sqrtvalue>0){
+        outputDiv.innerText=Math.trunc(sqrtvalue);
+    }
+    else
+   {
+    outputDiv.innerText="Enter non-negative value for computation";
+   }
+    
 }
 
 calculateHypotenuseBtn.addEventListener('click',calculateHypotenus);
